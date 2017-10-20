eebbfec4e0ded8f0fc6cef752e0c71c608064ef4
pareksha <pareksha.manchanda@gmail.com>
2017-10-20 23:32:05 +0530

    Decorators

---
+++ classes_examples/ex_4.py
@@ -0,0 +1,34 @@
+class Students:
+    def __init__(self,first,last):
+        self.first = first
+        self.last = last
+
+    @property
+    def email(self):
+        return "{}.{}@gmail.com".format(self.first,self.last)
+
+    @property
+    def fullname(self):
+        return "{} {}".format(self.first,self.last)
+
+    @fullname.setter
+    def fullname(self,name):
+        first,last = name.split(" ")
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print("name deleted")
+        self.first = None
+        self.last = None
+
+
+
+Anny = Students("Anny","Sullivan")
+Anny.fullname = "Pareksha Manchanda"
+print(Anny.first)
+print(Anny.email)
+
+del Anny.fullname
+print(Anny.fullname)
